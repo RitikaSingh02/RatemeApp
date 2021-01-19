f8694d1907da6eb13cf56ee672d0f840b00c9540
RitikaSingh02 <ritika2002singh@gmail.com>
2021-01-19 15:42:28 +0530

    added post route to the signup page

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,5 @@
+const passport = require("passport");
+
 module.exports = (app) => {
     app.get('/', (req, res, next) => {
         res.render('index', { 'title': "RATEmE ||INDEX" });
@@ -6,6 +8,12 @@ module.exports = (app) => {
     app.get('/user/signup', (req, res, next) => {
         res.render('user-login-signup/signup', { 'title': "SIGNUP" });
     })
+
+    app.post('/user/signup', passport.authenticate('local.usersignup', {
+        successRedirect: "/",
+        failureRedirect: "/user/signup",
+        failureFlash: true,
+    }));
 
     app.get('/user/login', (req, res, next) => {
         res.render('user-login-signup/login', { 'title': "LOGIN" });
